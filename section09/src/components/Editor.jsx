3c1709122903524294bc903752d94cdb0d9c60fd
Jeong Hoon <gbg9609@naver.com>
2024-05-11 22:23:54 +0900

    useReducer 학습, useReducer 사용 이유 : 코드 가독성을 높이기 위해, 상태관리 코드를 컴포넌트 외부에 작성함으로써 UI 부분 코드와 분리

---
+++ section09/src/components/Editor.jsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from "react";
+import "./Editor.css";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      alert("값을 입력 해주세요");
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+    alert("추가 되었습니다");
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        onChange={onChangeContent}
+        value={content}
+        onKeyDown={onKeyDown}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
---
+++ section09/src/components/List.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./List.css";
+import TodoItem from "./TodoItem";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  return (
+    <div className="List">
+      <h4>Todo List 🧸</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="Todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+            // 모든 아이템 컴포넌트에 고유한 key값을 설정 해주어야 함 why? 구별 해야됨. 그래서 id값을 넣어줌 id값이 각 리스트에 고유한 값을 나타내기 때문
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
